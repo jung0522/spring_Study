6dc6918623bc25720c6d928f3f0538165365db23
Teemo <jjy1111202@naver.com>
2024-09-30 21:04:20 +0900

    update

---
+++ HelloSpring/src/main/java/hello/HelloSpring/repository/memberRepository.java
@@ -0,0 +1,17 @@
+package hello.HelloSpring.repository;
+
+import hello.HelloSpring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+
+public interface memberRepository {
+
+    void clearStore();
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+
+}
