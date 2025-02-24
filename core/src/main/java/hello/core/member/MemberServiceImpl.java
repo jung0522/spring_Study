61a0b67b5c7978a7c967e3efe920f786e16bba68
jung0522 <jjy1111202@naver.com>
2025-02-24 16:35:27 +0900

    update

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,9 +1,11 @@
 package hello.core.member;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class MemberServiceImpl implements MemberService {
 
     // 인터체이스(MemberRepository) 참조 하는 구현체(MemoryMemberRepository) 생성
@@ -13,13 +15,22 @@ public class MemberServiceImpl implements MemberService {
     // 따라서 구페클래스를 생성하고 연결시켜주는 별도의 클래스 AppConfig 생성!!
 
     // 의존하는 인터페이스 선언만 해주고
+
+    // 3. 필드주입
+    //    @Autowired MemberRepository memberRepository;
     private final MemberRepository memberRepository;
 
-    // 생성자 주입
-    @Autowired // ac.getBean(MemberRepository.class)과 같은 역할
-    public MemberServiceImpl(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
+    // 1. 생성자 주입
+//    @Autowired // ac.getBean(MemberRepository.class)과 같은 역할
+//    public MemberServiceImpl(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+
+    // 2. setter 주입
+    //    @Autowired
+    //    public void setMemberRepository(MemberRepository memberRepository) {
+    //        this.memberRepository = memberRepository;
+    //    }
 
     @Override
     public void join(Member member) {
--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,11 +1,14 @@
 package hello.core.order;
 
+import hello.core.Annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -19,10 +22,10 @@ public class OrderServiceImpl implements OrderService {
     private final DiscountPolicy discountPolicy;
 
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
-    }
+   }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
